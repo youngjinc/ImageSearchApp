49ac257bb3400d3eba39b7b9de572a0ec2ec0c62
youngjinc <cyjin6789@gmail.com>
2023-03-21 20:03:16 +0900

    [feat] BindingActivity, BindingFragment 추가 및 적용

--- app/src/main/java/com/kakaobank/imagesearch/presentation/MainActivity.kt
+++ app/src/main/java/com/kakaobank/imagesearch/presentation/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.kakaobank.imagesearch.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.kakaobank.imagesearch.R
+import com.kakaobank.imagesearch.databinding.ActivityMainBinding
+import com.kakaobank.imagesearch.util.binding.BindingActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
     }
 }
--- app/src/main/java/com/kakaobank/imagesearch/presentation/search/SearchFragment.kt
+++ app/src/main/java/com/kakaobank/imagesearch/presentation/search/SearchFragment.kt
@@ -1,17 +1,14 @@
 package com.kakaobank.imagesearch.presentation.search
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.kakaobank.imagesearch.R
+import com.kakaobank.imagesearch.databinding.FragmentSearchBinding
+import com.kakaobank.imagesearch.util.binding.BindingFragment
 
-class SearchFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_search, container, false)
+class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
