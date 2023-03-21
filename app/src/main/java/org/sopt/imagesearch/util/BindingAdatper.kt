09be9bd04c7eda70c6df4aa978401553ce2cfc55
youngjinc <cyjin6789@gmail.com>
2023-03-21 17:52:59 +0900

    [feat] 메인 검색 뷰 구현

---
+++ app/src/main/java/org/sopt/imagesearch/util/BindingAdatper.kt
@@ -0,0 +1,12 @@
+package org.sopt.imagesearch.util
+
+import androidx.databinding.BindingAdapter
+import com.google.android.material.imageview.ShapeableImageView
+
+@BindingAdapter("radius")
+fun ShapeableImageView.setCornerRadius(radius: Float) {
+    val shapeAppearanceModel = this.shapeAppearanceModel.toBuilder()
+        .setAllCornerSizes(radius)
+        .build()
+    this.shapeAppearanceModel = shapeAppearanceModel
+}
