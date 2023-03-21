ad95673ba555f108729c9b9a525be98adb0f727a
youngjinc <cyjin6789@gmail.com>
2023-03-21 20:03:29 +0900

    [feat] 보관함뷰 구현

---
+++ app/src/main/java/com/kakaobank/imagesearch/presentation/archive/ArchiveFragment.kt
@@ -0,0 +1,14 @@
+package com.kakaobank.imagesearch.presentation.archive
+
+import android.os.Bundle
+import android.view.View
+import com.kakaobank.imagesearch.R
+import com.kakaobank.imagesearch.databinding.FragmentArchiveBinding
+import com.kakaobank.imagesearch.util.binding.BindingFragment
+
+class ArchiveFragment : BindingFragment<FragmentArchiveBinding>(R.layout.fragment_archive) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
