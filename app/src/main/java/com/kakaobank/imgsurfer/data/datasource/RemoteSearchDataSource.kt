d9a5dbc4f284520dafa041fac5d038a9863a9f7c
youngjinc <cyjin6789@gmail.com>
2023-03-22 23:17:04 +0900

    [del] 불필요한 파일 삭제

--- app/src/main/java/com/kakaobank/imgsurfer/data/datasource/RemoteSearchDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.kakaobank.imgsurfer.data.datasource
-
-import com.kakaobank.imgsurfer.data.service.SearchService
-import javax.inject.Inject
-
-class RemoteSearchDataSource @Inject constructor(
-    private val searchService: SearchService,
-)
