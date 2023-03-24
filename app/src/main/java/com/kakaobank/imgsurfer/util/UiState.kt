580ccebd5c15ca6e92b8e65315350e6906cc3be7
youngjinc <cyjin6789@gmail.com>
2023-03-25 01:56:57 +0900

    [del] 불필요한 파일 삭제

--- app/src/main/java/com/kakaobank/imgsurfer/util/UiState.kt
+++
@@ -1,9 +0,0 @@
-package com.kakaobank.imgsurfer.util
-
-sealed class UiState<out T> { // TODO 삭제 고려
-    object Init : UiState<Nothing>()
-    object Loading : UiState<Nothing>()
-    object Empty : UiState<Nothing>()
-    data class Success<T>(val data: T) : UiState<T>()
-    data class Error(val message: String?) : UiState<Nothing>()
-}
